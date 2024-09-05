f3f52fbb85dd840474d6b128b79e505030d86c7a
prommpd830 <andimayapada04@gmail.com>
2024-09-05 16:50:18 +0700

    add lightbox and add some filter format

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,6 @@
 
+		var api_url = "https://db.ygoprodeck.com/api/v7";
+
 		// AjaxStart
 		$(document).ajaxStart(function() {
 			$('#loading-cards').removeClass('d-none');
@@ -7,7 +9,110 @@
 		$(document).ajaxStop(function() {
 			$('#loading-cards').addClass('d-none');
 		});
+		
+		// ----
+		function removeForbiddenCharacters(string) {
+			let forbiddenChars = [' ']
 
+			for (let char of forbiddenChars){
+			    string = string.split(char).join('/');
+			}
+			return string
+		}
+
+		function isMobile() {
+			return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop|Mobile/i.test(navigator.userAgent);
+		}
+
+		// ----
+		
+
+		function loadLightbox() {
+			$('.lightbox-container img').simpleLightbox({
+				// default source attribute
+				sourceAttr: 'src',
+				// shows fullscreen overlay
+				overlay: true,
+				// shows loading spinner
+				spinner: true,
+				// shows navigation arrows
+				nav: (isMobile()) ? false : true,
+				// text for navigation arrows
+				navText: ['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+				// shows image captions
+				captions: false,
+				captionDelay: 0,
+				captionSelector: 'img',
+				captionType: 'attr',
+				captionPosition: 'bottom',
+				captionClass: '',
+				captionHTML: false,
+				// captions attribute (title or data-title)
+				captionsData: 'title',
+				// shows close button
+				close: true,
+				// text for close button
+				closeText: '<i class="fas fa-times"></i>',
+				// swipe up or down to close gallery
+				swipeClose: true,
+				// show counter
+				showCounter: true,
+				// file extensions
+				fileExt: 'png|jpg|jpeg|gif',
+				// weather to slide in new photos or not, disable to fade
+				animationSlide: true,
+				// animation speed in ms
+				animationSpeed: 100,
+				// image preloading
+				preloading: true,
+				// keyboard navigation
+				enableKeyboard: true,
+				// endless looping
+				loop:  true,
+				// group images by rel attribute of link with same selector
+				rel: false,
+				// closes the lightbox when clicking outside
+				docClose: false,
+				// how much pixel you have to swipe
+				swipeTolerance: 50,
+				// lightbox wrapper Class
+				className: 'simple-lightbox',
+				// width / height ratios
+				widthRatio: 0.8,
+				heightRatio: 0.9,
+				// scales the image up to the defined ratio size
+				scaleImageToRatio: false,
+				// disable right click
+				disableRightClick: false,
+				// show an alert if image was not found
+				alertError:  true,
+				// alert message
+				alertErrorMessage: 'Image not found, next image will be loaded',
+				// additional HTML showing inside every image
+				additionalHtml: false,
+				// enable history back closes lightbox instead of reloading the page
+				history: true,
+				// time to wait between slides
+				throttleInterval: 0,
+				// Pinch to <a href="https://www.jqueryscript.net/zoom/">Zoom</a> feature for touch devices
+				doubleTapZoom: 2,
+				maxZoom: 10,
+				// adds class to html element if lightbox is open
+				htmlClass: 'has-lightbox',
+				// RTL mode
+				rtl: false,
+				// elements with this class are fixed and get the right padding when lightbox opens
+				fixedClass: 'sl-fixed',
+				// fade speed in ms
+				fadeSpeed: 100,
+				// whether to uniqualize images
+				uniqueImages: true,
+				// focus the lightbox on open to enable tab control
+				focus: true,
+			});
+		}
+
+		// load click
 		function loadClick() {
 			// Get name card
 			$('#ygo .card-ygo').off('click');
@@ -19,17 +124,8 @@
 			})
 		}
 
-		function removeForbiddenCharacters(string) {
-			let forbiddenChars = [' ']
-
-			for (let char of forbiddenChars){
-			    string = string.split(char).join('/');
-			}
-			return string
-		}
-
 		// load more click
-		function moreClick(action, num, keyword, type, attribute){
+		function moreClick(action, num, keyword, type, attribute, format){
 			// Button See more
 			$('.more').off('click');
 			$('.more').click(function (e) {
@@ -37,12 +133,12 @@
 
 				num += 60;
 				if(action == 'loadAllCard'){
-					loadAllCard(num, type, attribute);
+					loadAllCard(num, type, attribute, format);
 				}else if(action == 'loadName'){
 					if(keyword === ''){
 						loadAllCard();
 					}else {
-						loadName(num, keyword, type, attribute);
+						loadName(num, keyword, type, attribute, format);
 					}
 				}else if(action == 'loadArchetype'){
 					if(keyword === ''){
@@ -57,7 +153,7 @@
 		}
 
 		// Get All Cards
-		function loadAllCard(num = 60, type = '', attribute = '') {
+		function loadAllCard(num = 60, type = '', attribute = '', format = '') {
 			$('#result-length').parent().removeClass('d-none');
 
 			// Filter Type
@@ -74,8 +170,15 @@
 				optionAttribute = '';
 			}
 
+			// Filter Format
+			let optionFormat = '';
+			optionFormat += 'format='+ format +'&';
+			if (optionFormat == 'format=&') {
+				optionFormat = '';
+			}
+
 			$.ajax({
-				url: `https://db.ygoprodeck.com/api/v7/cardinfo.php?${optionType}${optionAttribute}num=${num}&offset=0`,
+				url: `${api_url}/cardinfo.php?${optionType}${optionAttribute}${optionFormat}num=${num}&offset=0`,
 				type: 'get',
 				dataType: 'json',
 				success: (result) => {
@@ -87,7 +190,7 @@
 						} else {
 							$('.more').parent().addClass('d-none');
 						}
-						let url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${val.name}`;
+						let url = `${api_url}/cardinfo.php?name=${val.name}`;
 						$.each(val.card_images, function (i, val) {
 							$('#ygo').append(`
 								<div class="col-lg-2 col-md-3 col-6">
@@ -95,10 +198,14 @@
 									<img src="${val.image_url}" class="img-fluid my-2 card-ygo">
 								</div>
 								`)
+
+							if (i == 0) {
+								return false
+							}
 						})
 					})
 
-					moreClick('loadAllCard', num, '', type, attribute);
+					moreClick('loadAllCard', num, '', type, attribute, format);
 					loadClick();
 				},
 				error: (result) => {
@@ -114,7 +221,7 @@
 		}
 
 		// Search by Name card
-		function loadName(num = 60, keyword, type = '', attribute = '') {
+		function loadName(num = 60, keyword, type = '', attribute = '', format = '') {
 			$('#result-length').parent().removeClass('d-none');
 
 			// Filter Type
@@ -131,8 +238,15 @@
 				optionAttribute = '';
 			}
 
+			// Filter Format
+			let optionFormat = '';
+			optionFormat += 'format='+ format +'&';
+			if (optionFormat == 'format=&') {
+				optionFormat = '';
+			}
+
 			$.ajax({
-				url: `https://db.ygoprodeck.com/api/v7/cardinfo.php?${optionType}${optionAttribute}fname=${keyword}&num=${num}&offset=0`,
+				url: `${api_url}/cardinfo.php?${optionType}${optionAttribute}${optionFormat}fname=${keyword}&num=${num}&offset=0`,
 				type: 'get',
 				asyn: true,
 				dataType: 'json',
@@ -144,18 +258,24 @@
 						} else {
 							$('.more').parent().addClass('d-none');
 						}
-						let url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${val.name}`;
+						let url = `${api_url}/cardinfo.php?name=${val.name}`;
+						
 						$.each(val.card_images, function (i, val) {
+
 							$('#ygo').append(`
 								<div class="col-lg-2 col-md-3 col-6">
 									<span class="visually-hidden">${url}</span>
 									<img src="${val.image_url}" class="img-fluid my-2 card-ygo">
 								</div>
-								`)
+								`);
+							
+							if (i == 0) {
+								return false
+							}
 						})
 					})
 
-					moreClick('loadName', num, keyword, type, attribute);	
+					moreClick('loadName', num, keyword, type, attribute, format);	
 					loadClick();
 				},
 				error: (result) => {
@@ -175,14 +295,14 @@
 			$('#result-length').parent().removeClass('d-none');
 			$('.more').parent().addClass('d-none');
 			$.ajax({
-				url: `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${keyword}`,
+				url: `${api_url}/cardinfo.php?archetype=${keyword}`,
 				type: 'get',
 				asyn: true,
 				dataType: 'json',
 				success: (result) => {
 					$('#result-length').html(result.data.length);
 					$.each(result.data, function (i, val) {
-						let url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${val.name}`;
+						let url = `${api_url}/cardinfo.php?name=${val.name}`;
 						$.each(val.card_images, function (i, val) {
 							$('#ygo').append(`
 								<div class="col-lg-2 col-md-3 col-6">
@@ -190,6 +310,10 @@
 									<img src="${val.image_url}" class="img-fluid my-2 card-ygo">
 								</div>
 								`)
+
+							if (i == 0) {
+								return false
+							}
 						})
 					})
 
@@ -299,6 +423,21 @@
 						let def = val.def;
 						let archetype = val.archetype;
 						
+						let variant_img = ``;
+						$.each(val.card_images, function (i, card) {
+							if (i == 0) {
+								return;
+							}
+
+							variant_img += `<img src="${card.image_url}" class="mx-1 card-ygo-small" >`;
+						})
+
+						let container_variant_img = `
+							<div class="d-flex flex-nowrap overflow-auto my-2">
+								${variant_img}
+							</div>
+						`;
+
 						$.each(val.card_images, function (i, card) {
 							if (level) {
 								if(type.includes('Pendulum')) {
@@ -316,9 +455,10 @@
 
 
 									$('#ygo').append(`
-									<div class="col-lg-3 col-md-4 col-12 mb-4">
+									<div class="col-lg-3 col-md-4 col-12 mb-4 lightbox-container">
 										<span class="visually-hidden">${url}</span>
-										<img src="${card.image_url}" class="img-fluid d-block mx-auto card-ygo">
+										<img src="${card.image_url}" class="img-fluid d-block mx-auto">
+										${container_variant_img}
 									</div>
 									<div class="col-lg-9 col-md-8 col-12 px-md-4 px-3 mb-4">
 										<h2 class="display-6 fw-bold text-prompt text-hidden text-primary d-flex justify-content-between align-items-center">${name} ${attribute}</h2>
@@ -350,9 +490,10 @@
 									`);
 								} else {
 									$('#ygo').append(`
-									<div class="col-lg-3 col-md-4 col-12 mb-4">
+									<div class="col-lg-3 col-md-4 col-12 mb-4 lightbox-container">
 										<span class="visually-hidden">${url}</span>
-										<img src="${card.image_url}" class="img-fluid d-block mx-auto card-ygo">
+										<img src="${card.image_url}" class="img-fluid d-block mx-auto">
+										${container_variant_img}
 									</div>
 									<div class="col-lg-9 col-md-8 col-12 px-md-4 px-3 mb-4">
 										<h2 class="display-6 fw-bold text-prompt text-hidden text-primary d-flex justify-content-between align-items-center">${name} ${attribute}</h2>
@@ -372,9 +513,10 @@
 								}
 							} else {
 								$('#ygo').append(`
-									<div class="col-lg-3 col-md-4 col-12 mb-4">
+									<div class="col-lg-3 col-md-4 col-12 mb-4 lightbox-container">
 										<span class="visually-hidden">${url}</span>
-										<img src="${card.image_url}" class="img-fluid d-block mx-auto card-ygo">
+										<img src="${card.image_url}" class="img-fluid d-block mx-auto">
+										${container_variant_img}
 									</div>
 									<div class="col-lg-9 col-md-8 col-12 px-md-4 px-3 mb-4 position-relative">
 										<h2 class="display-6 fw-bold text-prompt text-hidden text-primary d-flex justify-content-between align-items-center">${name} ${attribute}</h2>
@@ -385,10 +527,15 @@
 									</div>
 									`);
 							}
-						})
-					})
 
-					loadClick();
+							if (i == 0) {
+								return false
+							}
+						})
+
+					})
+					
+					loadLightbox();
 				},
 				error: (result) => {
 					$('#result-length').parent().addClass('d-none');
@@ -410,6 +557,12 @@
 			$('input[name="keyword"]').data('keyword', search)
 			.attr('placeholder', placeholder)
 			.val('');
+
+			if (search == 'archetype') {
+				$('#filter').addClass('d-none');
+			} else {
+				$('#filter').removeClass('d-none');
+			}
 		})
 
 		// Form Keyword Submit
@@ -432,20 +585,84 @@
 			})
 			let remakeType = type.slice(0, type.length - 1);
 
+			let format = '';
+			$('#form-filter input[name="format"]:checked').each(function () {
+				format += $(this).val() +',';
+			})
+			let remakeFormat = format.slice(0, format.length - 1);
+
 			// If keyword null
 			if (keyword === "") {
-				loadAllCard(60, remakeType, remakeAttribute);
+				loadAllCard(60, remakeType, remakeAttribute, remakeFormat);
 				return false;
 			}
 			// Search for Name
 			if (search == 'name') {
-				loadName(60, keyword, remakeType, remakeAttribute);
+				loadName(60, keyword, remakeType, remakeAttribute, remakeFormat);
 			}
 			// Search fo Archtype
 			if (search == 'archetype') {
 				loadArchetype(keyword);
 			}
 		})
+		
+		// Form Filter Submit
+		$('.filter-submit').on('click', function(e) {
+			e.preventDefault();
+
+			$('#search').trigger('submit');
+		})
+
+		var timerSearch;
+		$('input[type="search"]').on('keyup', function(e){
+			clearTimeout(timerSearch);
+			timerSearch = setTimeout(() => {
+				$('#ygo').html('');
+				let keyword = $(this).val();
+				let search = $(this).data('keyword');
+
+				let attribute = '';
+				$('#form-filter input[name="attribute"]:checked').each(function () {
+					attribute += $(this).val() +',';
+				})
+				let remakeAttribute = attribute.slice(0, attribute.length - 1);
+
+				let type = '';
+				$('#form-filter input[name="type"]:checked').each(function () {
+					type += $(this).val() +',';
+				})
+				let remakeType = type.slice(0, type.length - 1);
+
+				let format = '';
+				$('#form-filter input[name="format"]:checked').each(function () {
+					format += $(this).val() +',';
+				})
+				let remakeFormat = format.slice(0, format.length - 1);
+
+				// If keyword null
+				if (keyword === "") {
+					loadAllCard(60, remakeType, remakeAttribute, remakeFormat);
+					return false;
+				}
+				// Search for Name
+				if (search == 'name') {
+					loadName(60, keyword, remakeType, remakeAttribute, remakeFormat);
+				}
+				// Search fo Archtype
+				if (search == 'archetype') {
+					loadArchetype(keyword);
+				}
+			}, 500);
+		})
+
+		// Change Logo YGO
+		// $('#form-filter input[name="format"]').on('change', function(e){
+
+		// 	let logo_url = $(this).parent().find('img').attr('src');
+
+		// 	$('.logo-ygo').attr('src', logo_url);
+		// })
 
 		loadAllCard();
+
 
